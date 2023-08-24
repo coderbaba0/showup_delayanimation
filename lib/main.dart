33ee50a90a2e3bec36865bc67edd1e4f8a845781
coderbaba <sattababakingup@gmail.com>
2023-08-24 19:05:42 +0530

    updated container animation
    add custom marquee with texj

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   Text("Texts second flutterfever.com",style: TextStyle(color: Colors.green,fontSize: 20)),
-                  Text("Texts   flutterfever.com",style: TextStyle(color: Colors.purple,fontSize: 15)),
+                  Text("Texts   flutterfever",style: TextStyle(color: Colors.purple,fontSize: 15)),
                 ],
               ),
               delay: delayAmount + 900,
